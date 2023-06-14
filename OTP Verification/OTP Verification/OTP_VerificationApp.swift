58fe71d19ff86307c2af4a69ed6914b3950322cf
Bonginkosi-BBT <64373437+Bonginkosi-BBT97@users.noreply.github.com>
2023-06-14 14:24:02 +0200

    Initial File setup

---
+++ OTP Verification/OTP Verification/OTP_VerificationApp.swift
@@ -0,0 +1,17 @@
+//
+//  OTP_VerificationApp.swift
+//  OTP Verification
+//
+//  Created by Bonginkosi Tshabalala on 2023/06/14.
+//
+
+import SwiftUI
+
+@main
+struct OTP_VerificationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
