ba9409a238b6ba22362aeb0e0456a97db92b8528
Bonginkosi-BBT <64373437+Bonginkosi-BBT97@users.noreply.github.com>
2023-06-15 12:59:06 +0200

    Updated ViewModel to cater for sending OTP

--- OTP Verification/OTP Verification/Shared/ViewModel/OTPViewModel.swift
+++ OTP Verification/OTP Verification/Shared/ViewModel/OTPViewModel.swift
@@ -11,4 +11,41 @@ import SwiftUI
 class OTPViewModel: ObservableObject {
     @Published var otpText: String = ""
     @Published var otpFields: [String] = Array(repeating: "", count: 6)
+    
+   //MARK: Login Data
+    @Published var number: String = ""
+    @Published var code: String = ""
+    
+    //MARK: Error
+    @Published var showAlert: Bool = false
+    @Published var errorMsg: String = ""
+    
+    //MARK: OTP Credentials
+    @Published var verificationCode: String = ""
+    @Published var isLoading: Bool = false
+    
+    
+    //MARK: Sending OTP
+    func sendOTP() async {
+        if isLoading {return}
+        do{
+            isLoading = true
+            let result = try await PhoneAuthProvider.provider().verifyPhoneNumber("+\(code)\(number)", uiDelegate: nil)
+            DispatchQueue.main.async {
+                
+                self.isLoading = false
+                self.verificationCode = result
+            }
+        } catch {
+            handleError(error: error.localizedDescription)
+        }
+    }
+    
+    func handleError(error: String) {
+        DispatchQueue.main.sync {
+            self.isLoading = false
+            self.errorMsg = error
+            self.showAlert.toggle()
+        }
+    }
 }
